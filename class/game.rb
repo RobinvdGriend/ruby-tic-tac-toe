21ee7e22f10b26ecad66d12e2611263f8ace942d
Robin van de Griend <robinvdgriend@outlook.com>
2015-09-26 11:15:35 +0200

    Add Game class and main script
    Add tic_tac_toe.rb as main entry point for the game which also contains
    all of the requires to make it easier to manage these.
    
    Add the Game class which will for now be the only class to contain any
    output. These output methods might later be moved to their own seperate
    module. Also all game constants like winning conditions will be put in
    the Game class as well.

--- class/game.rb
+++ class/game.rb
@@ -1,13 +1,67 @@
 class Game
+  WINNING_LINES = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 5]
+          ]
+  EXIT_COMMANDS = ["quit", "q"]
+
   def initialize
-
-  end
-
-  def create_player
-
+    @player_1 = create_player
+    @player_2 = create_player
+    @board = create_board
   end
 
   def run
+    print_welcome
+    loop do
+      print_board
 
+      input = get_input
+
+      if EXIT_COMMANDS.include?(input[0])
+        break
+      end
+    end
+  end
+
+  private
+
+  def create_player
+    Player.new
+  end
+
+  def create_board
+    Board.new
+  end
+
+  def print_welcome
+    puts "---------Welcome to Tic Tac Toe!---------"
+    puts "Made by: RobinvdGriend"
+    puts " Github: https://github.com/RobinvdGriend"
+    puts "-----------------------------------------"
+  end
+
+  def print_board
+    puts "+---+---+---+"
+    @board.tiles.each_slice(3) do |row|
+      print "|"
+      row.each do |mark|
+        if mark == nil
+          print "   |"
+        else
+          print " #{mark} |"
+        end
+      end
+      print "\n"
+      puts "+---+---+---+"
+    end
+  end
+
+  def get_input
+    puts "Please enter the coordinates in the field you want to mark (e.g. 2,3)"
+    puts "Enter \"quit\" or \"q\" to exit"
+    # TODO add error handling
+    gets.chomp.split(/\W+/)
   end
 end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,6 @@
 require_relative "class/game"
 require_relative "class/player"
-require_relative "class/field"
+require_relative "class/board"
+
+game = Game.new
+game.run
