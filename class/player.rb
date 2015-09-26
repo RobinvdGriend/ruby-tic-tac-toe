33485cec206ab67b5659b8cc55112f75b55d77d3
Robin van de Griend <robinvdgriend@outlook.com>
2015-09-26 11:00:11 +0200

    Add player class
    Initialize class with an integer id, a class variable containing the
    player count and a mark variable containing the symbol that the player
    will use to mark the board.
    
    Seperation of methods is such that all methods that need to act on a
    specific player are in the Player class (e.g. checking if a specific
    player has won). This will prevent (or at least minimize) passing around
    Player objects as arguments.

--- class/player.rb
+++ class/player.rb
@@ -1,5 +1,23 @@
 class Player
-  def place_mark(field object)
+  attr_accessor :id
+  attr_reader :mark
 
+  def initialize
+    @@player_count ||= 0
+    @@player_count += 1
+    @id = @@player_count
+    @mark = @@player_count == 1 ? :X : :O
+  end
+
+  def place_mark(coords, board)
+    board.fields[coords[0]][coords[1]] = @mark
+  end
+
+  def check_won(board)
+    lines = Game::WINNING_LINES
+    marked_tiles = board.tiles.each_index.select { |i| board.tiles[i] == @mark }
+    lines.any? do |line|
+      (line & marked_tiles) == line
+    end
   end
 end
